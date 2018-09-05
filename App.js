8916aee930419cfd4b4ee708fcc34e7869b0ad97
Rohan Singh <rohaniiitv@gmail.com>
2018-09-06 01:18:34 +0530

    further progress

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
   constructor(props) {
       super(props)
       this.state = {
-        currentScreen : 'landing',
+        currentScreen : 'search',
       }
   }
 
--- src/Search.js
+++ src/Search.js
@@ -1,29 +1,50 @@
 import React from 'react';
+import Loader from './Loader';
 import {StyleSheet, View, Text} from 'react-native';
-import { SearchBar } from 'react-native-elements';
+import { Container, Header, Item, Input, Icon, Button } from 'native-base';
 
 export default class Search extends React.Component {
+	state = {
+		pokeSearch : ' ',
+	}
+
+	searchPoke = () => {
+		let x = this.state.pokeSearch;
+		console.log("button tapped " + x);
+	}
+
 	render () {
 		return (
-					<View style={styles.container}>
-						<SearchBar
-						containerStyle={{width: '100%'}}
-						 lightTheme
-						 onChangeText={() => {}}
-						 onClearText={() => {}}
-						 icon={{type: 'font-awesome', name: 'search'}}
-						 placeholder="Search Pokemon...."/>					
-					</View>
+				<View style={styles.container}>
+			        <Header 
+			        searchBar
+			        rounded
+			        style={styles.header}>
+			          <Item>
+			            <Icon name="ios-search" onPress={this.searchPoke}/>
+			            <Input placeholder="Search"
+			             value = {this.state.pokeSearch}
+			             
+			             onChangeText = {(pokeSearch) => this.setState({pokeSearch})}
+			              />
+			          </Item>
+			        </Header>
+			        <Loader/>
+			     </View>
 			)
 	}
 }
 
 const styles = {
 
+	header : {
+		width : '100%',
+		height : 40,
+	},
+
 	container : {
 		flex : 1,
 		alignItems : 'center',
-		backgroundColor : '#ff6600',
 		width : '100%',
 		marginTop : 24,
 	}
